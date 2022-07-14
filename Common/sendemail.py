db4e4d72c0d54e810df1a7a13abedc97d96b48f7
zljun8210 <zljun8210@163.com>
2022-07-14 11:42:35 +0800

    more Mail box config

--- Common/sendemail.py
+++ Common/sendemail.py
@@ -10,6 +10,7 @@
    Change Activity:
                    9/13/2021: add
                    9/28/2021: 增加异常信息输出
+                   7/14/2022: 采用多组发件箱配置，邮件发送成功为止
 -------------------------------------------------
 """
 __author__ = 'ljzeng'
@@ -24,16 +25,16 @@ from selenium import webdriver
 import time
 import os
 from datetime import datetime
-from Common.logger import Log
+from logger import Log
 from selenium.webdriver.chrome.options import Options
 
-# log = Log()
-# path = ".\\report"
-# isExists = os.path.exists(path)
-# if not isExists:
-#     os.mkdir(path)
-#
-# log.logname = 'sendmail.log'
+log = Log()
+path = ".\\report"
+isExists = os.path.exists(path)
+if not isExists:
+    os.mkdir(path)
+
+log.logname = 'sendmail.log'
 
 def reportScreenshot(report):
     # 打开报告
@@ -52,24 +53,22 @@ def reportScreenshot(report):
     chdriver.set_window_size(width=w, height=h)
 
     try:
-        # log.info("open report, and screenshot.")
+        log.info("open report, and screenshot.")
         chdriver.save_screenshot('.\\report\\report.png')
         time.sleep(2)
     except:
         print("Screenshot fail")
-        # log.info("Screenshot fail")
+        log.info("Screenshot fail")
     chdriver.quit()
 
 def sendEmail(ver, report, runtime):
-    # 发件人
-    sender = 'xiaoer-82@tom.com'
-    sendPass = 'Xiaoer1982-'
-    server = 'smtp.tom.com'
-    port = 25
+    # 发件人，账号密码屏蔽
+    senders = [['********@tom.com', '********', 'smtp.tom.com', 'Tom邮箱'],
+               ['********@sina.cn', '********', 'smtp.sina.com', '新浪邮箱']]
 
     # 收件人
-    receivers = ['LJZeng@foresightintelligence.com','LWang@foresightintelligence.com','llj@foresightintelligence.com','xiongdh79@126.com']   # 可设置多个收件人
-    cc = ['QHong@foresightintelligence.com','DCHu@foresightintelligence.com','zljun8210@163.com']
+    receivers = ['LJZeng@foresightintelligence.com', 'LWang@foresightintelligence.com', 'llj@foresightintelligence.com', 'xiongdh79@126.com']   # 可设置多个收件人
+    cc = ['QHong@foresightintelligence.com', 'DCHu@foresightintelligence.com', 'zljun8210@163.com']
     # receivers = ['LJZeng@foresightintelligence.com']
     # cc = ['zljun8210@163.com']
 
@@ -114,67 +113,76 @@ def sendEmail(ver, report, runtime):
 </body>    
 </html>
     """
-    message = MIMEMultipart()
-    subject = '%s 版本自动化测试结果' % ver
-    message['Subject'] = Header(subject, 'utf-8')
-    message['From'] = sender
-    message['To'] = ";".join(receivers)
-    message['Cc'] = ";".join(cc)
-    # 邮件正文
-    mail_msg = mail_msg.format(version=ver, runtime=runtime)
-    message.attach(MIMEText(mail_msg, 'html', 'utf-8'))
 
-    # 报告截图
-    try:
-        # 打开报告并截图
-        reportScreenshot(report)
-        time.sleep(3)
-        fp = open('.\\report\\report.png', 'rb')
-        messageImage = MIMEImage(fp.read())
-        fp.close()
-        messageImage.add_header('Content-ID', '<image>')
-        message.attach(messageImage)
-        # log.info("get test report screenshot")
-        os.system("del .\\report\\report.png")
-    except Exception as e:
-        messageImage = None
-        messageImage.add_header('Content-ID', '<image>')
-        message.attach(messageImage)
-        print(e.args)
-        # log.info("Failed to get screenshot!")
+    # 配置多个发件箱，当发送失败时，使用下一组发件箱配置
+    for ser in senders:
+        sender = ser[0]
+        sendPass = ser[1]
+        server = ser[2]
+        name = ser[3]
 
-    # 邮件附件
-    try:
-        # file = r'.\\report\\result_2021.09.10.203036.115\\result_2021.09.10.203036.115.html'
-        attRe = MIMEText(open(report, 'rb').read(), 'html', 'utf-8')
-        attRe["Content-Type"] = 'text/html'
-        # attRe["Content-Disposition"] = 'attachment; filename"TestReport.html" '
-        # 中文附件名处理方式
-        attRe.add_header('Content-Disposition', 'attachment', filename=('gbk', '', '%s 版本测试报告.html' % ver))
-        encoders.encode_base64(attRe)
-        message.attach(attRe)
-        time.sleep(3)
-        # log.info("Attachment loaded successfully！！")
-    except Exception as e:
-        print(e.args)
-        # log.info("Attachment loaded failer!")
+        message = MIMEMultipart()
+        subject = '%s 版本自动化测试结果' % ver
+        message['Subject'] = Header(subject, 'utf-8')
+        message['From'] = sender
+        message['To'] = ";".join(receivers)
+        message['Cc'] = ";".join(cc)
+        # 邮件正文
+        mail_msg = mail_msg.format(version=ver, runtime=runtime)
+        message.attach(MIMEText(mail_msg, 'html', 'utf-8'))
 
-    # 发送邮件
-    try:
-        # ser = smtplib.SMTP_PORT(server, port)   # 发件人邮箱的SMTP服务器，端口
-        ser = smtplib.SMTP(server)
-        ser.login(sender, sendPass)    # 发件人账号，密码
-        ser.sendmail(sender, receivers + cc, message.as_string())    # 发件人邮箱、收件人邮箱、邮件内容
-        time.sleep(5)
-        ser.quit()    # 关闭连接
-        # log.info("Mail sent successfully！！")
-    except Exception as e:
-        print(e.args)
-        # log.info("Failed to send mail")
+        # 报告截图
+        try:
+            # 打开报告并截图
+            reportScreenshot(report)
+            time.sleep(3)
+            fp = open('.\\report\\report.png', 'rb')
+            messageImage = MIMEImage(fp.read())
+            fp.close()
+            messageImage.add_header('Content-ID', '<image>')
+            message.attach(messageImage)
+            log.info("get test report screenshot")
+            os.system("del .\\report\\report.png")
+        except Exception as e:
+            messageImage = None
+            messageImage.add_header('Content-ID', '<image>')
+            message.attach(messageImage)
+            log.info(e.args)
+            log.info("Failed to get screenshot!")
+
+        # 邮件附件
+        try:
+            attRe = MIMEText(open(report, 'rb').read(), 'html', 'utf-8')
+            attRe["Content-Type"] = 'text/html'
+            # 中文附件名处理方式
+            attRe.add_header('Content-Disposition', 'attachment', filename=('gbk', '', '%s 版本测试报告.html' % ver))
+            encoders.encode_base64(attRe)
+            message.attach(attRe)
+            time.sleep(3)
+            log.info("Attachment loaded successfully！！")
+        except Exception as e:
+            log.info(e.args)
+            log.info("Attachment loaded failer!")
+
+        # 发送邮件
+        try:
+            # ser = smtplib.SMTP_PORT(server, port)   # 发件人邮箱的SMTP服务器，端口
+            ser = smtplib.SMTP(server)
+            ser.login(sender, sendPass)    # 发件人账号，密码
+            ser.sendmail(sender, receivers + cc, message.as_string())    # 发件人邮箱、收件人邮箱、邮件内容
+            time.sleep(5)
+            ser.quit()    # 关闭连接
+        except Exception as e:
+            log.info(e.args)
+            log.info("%s: Failed to send mail" % name)
+            continue
+        else:
+            log.info("%s: Mail sent successfully！！" % name)
+            break
 
 if __name__ == '__main__':
     nowtime = datetime.now().strftime("%Y.%m.%d %H:%M")
     version = '2.21.908'
-    reoprtf = r'./report/result_2022.01.18.200016.601.html'
-    # sendEmail(version, reoprtf, runtime=nowtime)
-    reportScreenshot(reoprtf)
+    reoprtf = r'./report/result_2022.06.27.162346.843.html'
+    sendEmail(version, reoprtf, runtime=nowtime)
+    # reportScreenshot(reoprtf)
